691cb9c7077e3a35435fc463a9a0e70888c21e0f
cocosaso <saso.drone1998@gmail.com>
2024-06-24 15:53:17 +0900

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import QuizGame from './QuizGame';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <QuizGame />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
